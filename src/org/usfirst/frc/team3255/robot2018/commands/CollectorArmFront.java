a414d7aaa005a8b9f10b838562d6a622460df863
Driverstation 1 <frcteam3255@gmail.com>
2018-03-17 16:48:25 -0700

    Made ArmMove Execute Instead of Initalize

--- src/org/usfirst/frc/team3255/robot2018/commands/CollectorArmFront.java
+++ src/org/usfirst/frc/team3255/robot2018/commands/CollectorArmFront.java
@@ -18,11 +18,11 @@ public class CollectorArmFront extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.collector.setArmSpeed(RobotPreferences.collectorArmSpeed());
     }
     
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {	
+    	Robot.collector.setArmSpeed(RobotPreferences.collectorArmSpeed());
     }
     
     // Make this return true when this Command no longer needs to run execute()
